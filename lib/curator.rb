cef05d854f3dbca3e23e5de59660e5234cb7c03f
DanHutch <danhutch5280@gmail.com>
2018-09-18 09:55:22 -0600

    finished iteration 2, retrying push

--- lib/curator.rb
+++ lib/curator.rb
@@ -13,5 +13,20 @@ class Curator
     @photographs << Photograph.new(photo)
   end
 
+  def add_artist(artist)
+    @artists << Artist.new(artist)
+  end
+
+  def find_artist_by_id(id)
+    @artists.find do |artist|
+      artist.id == id
+    end
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find do |photograph|
+      photograph.id == id
+    end
+  end
 
 end
